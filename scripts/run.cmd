8e3453e34867c9480de03bb9d2121a8fef0e317b
defin85 <31535407+defin85@users.noreply.github.com>
2025-12-01 12:32:28 +0300

    feat: удалить параметр адреса 0.0.0.0 из команды создания службы

--- scripts/run.cmd
+++ scripts/run.cmd
@@ -2,5 +2,5 @@ chcp 65001
 set V8=C:\Program Files\1cv8\8.3.27.1786\bin
 set NameSrvc=ras_1539
 set DisplName="Сервер администрирования 1С:Предприятия 8.3 (x86-64) (ragent:1540)"
-set Command="\"%V8%\ras.exe\" --service cluster --address 0.0.0.0 -p 1539 127.0.0.1:1540"
+set Command="\"%V8%\ras.exe\" --service cluster -p 1539 127.0.0.1:1540"
 sc create %NameSrvc% binPath= %Command% start= auto obj= .\USR1CV8 password= p-123456 DisplayName= %DisplName% depend= Tcpip/Dnscache/lanmanworkstation/lanmanserver/
